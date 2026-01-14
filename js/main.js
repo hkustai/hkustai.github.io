cf66b174912bb49cb240fcf8c524bcc23f257595
yeahjack <yxu409@connect.hkust-gz.edu.cn>
2026-01-14 22:18:21 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -2,8 +2,22 @@
 //  $('.s-bg-image').height($(window).height());
 // })
 //
-$(".navbar a").click(function(){
-  $("body,html").animate({
-   scrollTop:$("#" + $(this).data('value')).offset().top-100
-  },1000)
- })
+$(".navbar a").click(function (e) {
+  var targetId = $(this).data("value");
+  if (!targetId) {
+    return; // 没有 data-value，交给默认行为
+  }
+
+  var $target = $("#" + targetId);
+  if ($target.length === 0) {
+    return; // 页面不存在目标元素
+  }
+
+  e.preventDefault();
+
+  $("body, html").animate(
+    { scrollTop: $target.offset().top - 100 },
+    1000
+  );
+});
+
